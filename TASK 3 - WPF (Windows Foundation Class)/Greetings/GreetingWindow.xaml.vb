e1096ed13f686c86387771f9f8ce4b3f01241fe6
nadyapermaata <79756684+nadyapermaata@users.noreply.github.com>
2022-03-01 08:47:53 +0700

    Add files via upload

---
+++ TASK 3 - WPF (Windows Foundation Class)/Greetings/GreetingWindow.xaml.vb
@@ -0,0 +1,5 @@
+﻿Public Class GreetingWindow
+    Private Sub TextBox_TextChanged(sender As Object, e As TextChangedEventArgs)
+
+    End Sub
+End Class
---
+++ TASK 3 - WPF (Windows Foundation Class)/Greetings/MainWindow.xaml.vb
@@ -0,0 +1,17 @@
+﻿Class MainWindow
+    Private Sub TextBox_TextChanged(sender As Object, e As TextChangedEventArgs)
+
+    End Sub
+
+    Private Sub TextBoxGreeting_GotFocus(sender As Object, e As RoutedEventArgs) Handles TextBoxGreeting.GotFocus
+        If (TextBoxGreeting.Text = "Tuliskan Greeting Anda disini!") Then
+            TextBoxGreeting.Text = ""
+        End If
+    End Sub
+
+    Private Sub ButtonSubmit_Click(sender As Object, e As RoutedEventArgs) Handles ButtonSubmit.Click
+        Dim Form2 As GreetingWindow = New GreetingWindow()
+        Form2.LabelGreeting.Text = TextBoxGreeting.Text
+        Form2.Show()
+    End Sub
+End Class
